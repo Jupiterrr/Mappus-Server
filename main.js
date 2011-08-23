00678878bc63a48faca5cbc94fd74d046f4140e0
Carsten Griesheimer <sun_od@hotmail.com>
2011-08-23 18:12:32 +0200

    .......

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-var _ = require("underscore");
-var redis = require("redis");
-var http = require('http');  
-var io = require('socket.io');
+// var _ = require("underscore");
+// var redis = require("redis");
+// var http = require('http');  
+// var io = require('socket.io');
+// 
+// var server = null;
+// 
+// 
+// redis = redis.createClient();
+// redis.on("error", function (err) {
+//     console.log("Error " + err);
+// });
+// 
+// 
+// server = http.createServer(httpRequest);
+// server.listen(process.env.PORT || 3000, "mappus.heroku.com");
+// 
+// 
+// function httpRequest(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'}); 
+//     setTimeout(function(){
+//         res.end('<h1>Hello world</h1>');
+//     }, 5000)
+// }
 
-var server = null;
+var express = require('express');
 
+var app = express.createServer(express.logger());
 
-redis = redis.createClient();
-redis.on("error", function (err) {
-    console.log("Error " + err);
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-
-server = http.createServer(httpRequest);
-server.listen(process.env.PORT || 3000, "mappus.heroku.com");
-
-
-function httpRequest(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'}); 
-    setTimeout(function(){
-        res.end('<h1>Hello world</h1>');
-    }, 5000)
-}
-
-
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 // // socket.io 
 // var socket = io.listen(server); 
